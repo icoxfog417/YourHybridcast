2a74cbee9fb34d06ce8bfcb48611db0567bc98d1
icoxfog417 <icoxfog417@yahoo.co.jp>
2014-06-01 17:44:13 +0900

    add minimum implementation

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿//main script
+
+var yhc = (function(){
+    var self = {};
+
+    self.PLAYER = "#player-api";
+    
+    self.setContent = function () {
+        var player = $(self.PLAYER);
+
+        chrome.storage.sync.get({
+            targetSite: 'https://icoxfog417.github.io/YourHybridcast/'
+        }, function (items) {
+            var $overlay = $("<iframe src='" + items.targetSite + "'></iframe>");
+            $overlay.width(player.width());
+            $overlay.height(player.height() - 38);
+            $overlay.css({ "position": "absolute", "top": player.position().top, "left": player.position().left });
+            $(body).append($overlay);
+        });
+
+    }
+
+    return self;
+
+})();
+
+$(function () {
+    
+    yhc.setContent();
+
+})
---
+++ options.js
@@ -0,0 +1,32 @@
+﻿// Saves options to chrome.storage
+function save_options() {
+    var options = {};
+    $(".option").each(function () {
+        options[$(this).attr("id")] = $(this).val();
+    })
+
+  chrome.storage.sync.set(options, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    targetSite: 'https://icoxfog417.github.io/YourHybridcast/'
+  }, function (items) {
+    $(".option").each(function () {
+        $(this).val(items[$(this).attr("id")]);
+    })
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
